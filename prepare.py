482dea239b320f0d05ab57c1a6941174d02fb484
rachelrobbinsmayhill <rachel.robbinsmayhill@gmail.com>
2022-03-14 15:39:58 -0500

    Complete

--- prepare.py
+++ prepare.py
@@ -57,3 +57,21 @@ def prep_telco(df):
                          drop_first = [True, True, True])
     df = pd.concat([df, dummy_df], axis=1)
     return df
+
+
+def train_validate_test_split(df, target, seed=123):
+    '''
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
+    '''
+    train_validate, test = train_test_split(df, test_size=0.2, 
+                                            random_state=seed, 
+                                            stratify=df[target])
+    train, validate = train_test_split(train_validate, test_size=0.3, 
+                                       random_state=seed,
+                                       stratify=train_validate[target])
+    return train, validate, test
